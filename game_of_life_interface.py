f33963a3c8abdaa394c7ab2b26aac32d3722da0f
Roberto Cruz Lozano <47866280+Tank3-TK3@users.noreply.github.com>
2021-04-08 12:55:14 -0500

    the interface was separated from the main code

--- game_of_life_interface.py
+++ game_of_life_interface.py
@@ -5,79 +5,126 @@
 ################################################################################
 
 ################################################################################
-#                   MODULES
+#                               MODULES
 
 import tkinter as tk
-import tkinter.font
-from game_of_life_main_class import GameOfLife
+from tkinter import font
 
 ################################################################################
-#                   MAIN
+#                                CLASS
 
-if __name__ == '__main__':
+class GameOfLifeInterface(tk.Frame):
+    root = None
+    frameOne = None
+    txtBoard = None
+    fontTitles = None
+    fontText = None
+    board = None
+    canvasBoard = None
+    frameButtons = None
+    buttons = None
+    frameInformation = None
+    txtInformation = None
 
-    window=tk.Tk()
-    window.title("Game of Life")
-    window.geometry("1280x720")
-    window.iconbitmap("./img/Glider.ico")
-    window.config(background="#FFFFFF")
-    window.resizable(width=False,height=False)
+    def __init__(self, root = None):
+        tk.Frame.__init__(self, root)
+        self.root = self.drawRoot(root)
+        self.fontTitles = tk.font.Font(family = "Arial", size = 25)
+        self.fontText = tk.font.Font(family = "Arial", size = 15)
+        self.frameOne = self.drawFrameOne()
+        self.txtBoard = self.drawTextBoard()
+        self.board = self.drawBoard()
+        self.canvasBoard = self.drawCanvasBoard()
+        self.frameButtons = self.drawFrameButtons()
+        self.buttons = self.drawButtons()
+        self.frameInformation = self.drawFrameInformation()
+        self.txtInformation = self.drawTextInformation()
 
-    fontTitles = tk.font.Font(family="Arial", size=25)
-    fontText = tk.font.Font(family="Arial", size=10)
+    def drawRoot(self,root):
+        root.title("Game of Life (v1.0)")
+        root.geometry("600x700+50+50")
+        root.iconbitmap("./img/Glider.ico")
+        root.config(background = "#FFFFFF")
+        root.resizable(width = False, height = False)
+        return root
 
-    frameLeft = tk.Frame(window,background="#22232D")
-    frameLeft.pack_propagate(0)
-    frameLeft.pack(fill='both',side='left',expand='True')
+    def drawFrameOne(self):
+        frameOne = tk.Frame(self.root, background = "#22232D")
+        frameOne.pack_propagate(0)
+        frameOne.pack(fill = 'both', side = 'left', expand = 'True')
+        return frameOne
 
-    txtBoard = tk.Label(frameLeft,foreground="#FFFFFF",background="#22232D",text="Game of Life",font=fontTitles)
-    txtBoard.pack_propagate(0)
-    txtBoard.pack()
+    def drawTextBoard(self):
+        txtBoard = tk.Label(self.frameOne, foreground = "#FFFFFF", background = "#22232D")
+        txtBoard.config(text = "Game of Life", font = self.fontTitles)
+        txtBoard.pack_propagate(0)
+        txtBoard.pack()
+        return txtBoard
 
-    board = tk.Frame(frameLeft,background="#FFFFFF",width="500",height="500",bd=0)
-    board.pack_propagate(0)
-    board.pack(side='top',padx=0,pady=0)
+    def drawBoard(self):
+        board = tk.Frame(self.frameOne,background="#FFFFFF",width="500",height="500",bd=0)
+        board.pack_propagate(0)
+        board.pack(side='top',padx=0,pady=0)
+        return board
 
-    canvasBoard = tk.Canvas(board,background="#666B8A",bd=0)
-    canvasBoard.pack_propagate(0)
-    canvasBoard.pack(fill='both',side='left',expand='True')
-    for i in range(0,500,20):
-        canvasBoard.create_line(i,0,i,500,fill="#FFFFFF")
-        canvasBoard.create_line(0,i,500,i,fill="#FFFFFF")
+    def drawCanvasBoard(self):
+        canvasBoard = tk.Canvas(self.board, background = "#666B8A")
+        canvasBoard.config(bd = 0, highlightthickness = 1, relief = 'ridge')
+        canvasBoard.pack_propagate(0)
+        canvasBoard.pack(fill = 'both', side = 'top', expand = 'True')
+        for i in range(20, 500, 20):
+            canvasBoard.create_line(i, 0, i, 500, fill = "#FFFFFF")
+            canvasBoard.create_line(0, i, 500, i, fill = "#FFFFFF")
+        return canvasBoard
 
-    frameLeftOne = tk.Frame(frameLeft,background="#22232D",width="500",height="100")
-    frameLeftOne.pack_propagate(0)
-    frameLeftOne.pack(side='top',padx=0,pady=20)
-
-    buttonStart = tk.Button(frameLeftOne,text="Start",font=fontText,background="#419A61",foreground="#FFFFFF")
-    buttonStart.pack_propagate(0)
-    buttonStart.pack(fill='both',side='left',expand='True',padx=5,pady=10)
-
-    buttonStop = tk.Button(frameLeftOne,text="Stop",font=fontText,background="#615391",foreground="#FFFFFF")
-    buttonStop.pack_propagate(0)
-    buttonStop.pack(fill='both',side='left',expand='True',padx=5,pady=10)
-
-    buttonClean = tk.Button(frameLeftOne,text="Clean",font=fontText,background="#756FA1",foreground="#FFFFFF")
-    buttonClean.pack_propagate(0)
-    buttonClean.pack(fill='both',side='left',expand='True',padx=5,pady=10)
-
-    buttonExit = tk.Button(frameLeftOne,text="Exit",font=fontText,background="#FE90CD",foreground="#000000")
-    buttonExit.pack_propagate(0)
-    buttonExit.pack(fill='both',side='left',expand='True',padx=5,pady=10)
-
-    #frameRight = tk.Frame(window,background="#282A36")
-    #frameRight.pack_propagate(0)
-    #frameRight.pack(fill='both',side='right',expand='True')
-
-    gol = GameOfLife(window,canvasBoard)
+    def drawFrameButtons(self):
+        frameButtons = tk.Frame(self.frameOne, background = "#22232D")
+        frameButtons.config(width = "500", height = "100")
+        frameButtons.pack_propagate(0)
+        frameButtons.pack(side='top',padx=0,pady=10)
+        return frameButtons
     
-    ################################################################################
-    #                   EVENTS
+    def drawButtons(self):
+        buttons = [["Start", "#6CD987", "#121212"], ["Stop", "#D977B7", "#121212"],
+                   ["Clean", "#837ACC", "#121212"], ["Exit", "#D3A16B", "#121212"]]
+        for i in buttons:
+            i.append(tk.Button(self.frameButtons, text = i[0], font = self.fontText))
+            i[3].config(background = i[1], foreground = i[2])
+            i[3].pack_propagate(0)
+            i[3].pack(fill = 'both', side = 'left', expand = 'True', padx = 5, pady = 20)
+        return buttons
 
-    canvasBoard.bind("<Button-1>",gol.clickMouseButton)
-    canvasBoard.bind("<Button-3>",gol.clickMouseButton)
-    buttonStart.config(command=gol.clickStartButton)
-    buttonClean.config(command=gol.clickCleanButton)
-    buttonExit.config(command=gol.clickExitButton)
+    def drawFrameInformation(self):
+        frameInformation = tk.Frame(self.frameOne, background = "#22232D")
+        frameInformation.config(height = "25")
+        frameInformation.pack_propagate(0)
+        frameInformation.pack(fill = 'x', side = 'bottom', padx = 0, pady = 0)
+        return frameInformation
 
-    window.mainloop()
+    def drawTextInformation(self):
+        txtInformation = [["v1.0", "right"], ["Coded by Tank3", "left"]]
+        for i in txtInformation:
+            i.append(tk.Label(self.frameInformation, foreground = "#FFFFFF", background = "#22232D"))
+            i[2].config(text = i[0], font = self.fontText)
+            i[2].pack_propagate(0)
+            i[2].pack(side = i[1])
+        return txtInformation
+
+    def aliveCell(self,j):
+        if j[7] == None or j[8] == None:
+            j[7] = self.canvasBoard.create_rectangle(j[1], j[2], j[3], j[4])
+            j[8] = self.canvasBoard.create_rectangle(j[1] + 5, j[2] + 5, j[3] - 5, j[4] - 5)
+            self.canvasBoard.itemconfig(j[7], fill = "#FFFFFF", outline = "#FFFFFF")
+            self.canvasBoard.itemconfig(j[8], fill = "#000000", outline = "#000000")
+        else:
+            self.canvasBoard.itemconfig(j[7], fill = "#FFFFFF", outline = "#FFFFFF")
+            self.canvasBoard.itemconfig(j[8], fill = "#000000", outline = "#000000")
+        j[5] = 1
+
+    def deadCell(self,j):
+        self.canvasBoard.itemconfig(j[7], fill = "#666B8A", outline = "#FFFFFF")
+        self.canvasBoard.itemconfig(j[8], fill = "#666B8A", outline = "#666B8A")
+        j[5] = 0
+
+    def __del__(self):
+        return 0
