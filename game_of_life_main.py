ad0595e732fad9ba1d365bc1ae3b1fe0dcc4cfb9
Roberto Cruz Lozano <47866280+Tank3-TK3@users.noreply.github.com>
2021-04-08 22:17:26 -0500

    update main

--- game_of_life_main.py
+++ game_of_life_main.py
@@ -18,12 +18,19 @@ def events():
     app.canvasBoard.bind("<Button-1>",logi.clickMouseButton)
     app.canvasBoard.bind("<Button-3>",logi.clickMouseButton)
     app.buttons[0][3].config(command = logi.clickStartButton)
+    app.buttons[1][3].config(command = logi.clickStopButton)
     app.buttons[2][3].config(command = logi.clickCleanButton)
     app.buttons[3][3].config(command = root.destroy)
+
+def game():
+    if logi.status == True:
+        logi.playGame()
+    root.after(125,game)
 
 if __name__ == '__main__':
     root = tk.Tk()
     app = GameOfLifeInterface(root)
     logi = GameOfLifeClass(app)
     events()
+    root.after(125, game)
     root.mainloop()
